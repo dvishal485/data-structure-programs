b440ebfd8ce73fdbab220aa826a8b7f89aa86d6a
Vishal Das <dvishal485@gmail.com>
2022-08-25 08:58:44 +0530

    added program 01
    Signed-off-by: Vishal Das <dvishal485@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# DTU CO101/CO102 Lab File
+
+Delhi Technological University Lab Programs for Data Structures (Second Year) for the course CO201 submitted by Vishal Das
+
+---
+
+## Programs included in the file
+
+1. Write a C/C++ program to Implement Linear Search.
+
+---
---
+++ programs/program_01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int array[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+        if (array[i] == key)
+            return i;
+    return -1;
+}
+
+int main(void)
+{
+    int size, key;
+    cout << "Input the size of array : ";
+    cin >> size;
+    int arr[size];
+    cout << "Input the " << size << " elements : ";
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    cout << "Element to search for : ";
+    cin >> key;
+    int index = linearSearch(arr, size, key);
+    if (index != -1)
+        cout << "Element found at index " << index << endl;
+    else
+        cout << "Element doesn't exist in the array" << endl;
+    return 0;
+}
