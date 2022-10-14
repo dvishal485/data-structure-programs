f5e578ef5b6d010001598758e6b8d9a3c71fb406
Vishal Das <dvishal485@gmail.com>
2022-10-14 12:56:51 +0530

    fix program_09.c
    Now takes input of whole matrix rather than the sparse matrix itself

--- programs/program_09.c
+++ programs/program_09.c
@@ -11,14 +11,25 @@ typedef struct {
 
 int main(void) {
     element a[MAX];
+    a[0].value = 0;
     printf("Enter the number of rows and columns of the matrix : ");
     scanf("%d %d", &a[0].row, &a[0].col);
-    printf("Enter the non zero elements of the matrix : ");
-    scanf("%d", &a[0].value);
+    printf("Enter the elements of the matrix :\n");
+    int x = 0;
+    for(int i=0; i<a[0].row; i++) {
+        for(int j=0; j<a[0].col; j++) {
+            scanf("%d", &x);
+            if(x!=0) {
+                a[0].value++;
+                a[a[0].value].row = i+1;
+                a[a[0].value].col = j+1;
+                a[a[0].value].value = x;
+            }
+        }
+    }
+
+    printf("\nGiven Sparse Matrix is represented as :\n");
     TABLE;
-    for (int i = 1; i <= a[0].value; i++)
-        scanf("%d %d %d", &a[i].row, &a[i].col, &a[i].value);
-    printf("Given Sparse Matrix is represented as :\n");
     for (int i = 1; i <= a[0].value; i++)
         printf("%d\t%d\t%d\n", a[i].row, a[i].col, a[i].value);
     return 0;
