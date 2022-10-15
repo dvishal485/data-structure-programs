961aa723424e04d85ebe8691202206bbd1694dbb
Vishal Das <dvishal485@gmail.com>
2022-10-15 13:36:31 +0530

    added Binary Tree implementation
    Signed-off-by: Vishal Das <dvishal485@gmail.com>

---
+++ programs/bt_impl.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct btNode {
+    int value;
+    struct btNode *left;
+    struct btNode *right;
+} btNode;
+
+btNode *createBtNode(int value) {
+    btNode *x = (btNode *)malloc(sizeof(btNode));
+    x->value = value;
+    x->left = NULL;
+    x->right = NULL;
+    return x;
+}
+
+void inOrderTraversal(btNode *root) {
+    if (!root) {
+        return;
+    } else {
+        if (root->left)
+            inOrderTraversal(root->left);
+        printf("%d, ", root->value);
+        if (root->right)
+            inOrderTraversal(root->right);
+    }
+}
+
+void btCountNodes(btNode *root, int *internal, int *leaf) {
+    if (!root) {
+        // null child
+        *internal += 0;
+        *leaf += 0;
+    } else if (!root->left && !root->right) {
+        // leaf node
+        *leaf += 1;
+    } else {
+        // internal node
+        *internal += 1;
+        btCountNodes(root->left, internal, leaf);
+        btCountNodes(root->right, internal, leaf);
+    }
+}
+
+int btHeight(btNode *root) {
+    if (!root || (!root->left && !root->right))
+        return 0;
+    int h1 = btHeight(root->left);
+    int h2 = btHeight(root->right);
+    return (h1 > h2) ? h1 + 1 : h2 + 1;
+}
